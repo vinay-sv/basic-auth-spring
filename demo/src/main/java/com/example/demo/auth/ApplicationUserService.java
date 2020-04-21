72567bf6ac1d0ffa4d6ea86779f1719a02228505
visweswa <visweswa@adobe.com>
2020-04-21 23:06:24 +0530

    Added the qualifier name

--- demo/src/main/java/com/example/demo/auth/ApplicationUserService.java
+++ demo/src/main/java/com/example/demo/auth/ApplicationUserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,7 +12,7 @@ public class ApplicationUserService implements UserDetailsService {
 
     private final ApplicationUserDao applicationUserDao;
 
-    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) { //TODO: Find out how to provide the right qualifier in case of multiple implementations of the dao class
         this.applicationUserDao = applicationUserDao;
     }
 
